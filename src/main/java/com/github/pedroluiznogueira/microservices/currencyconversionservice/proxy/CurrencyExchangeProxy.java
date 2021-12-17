28dafc501e33bc9723f7fe28b0fd900d97d1edde
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-12-16 21:24:24 -0300

    Using feign to fetch currency exchange microservice

---
+++ src/main/java/com/github/pedroluiznogueira/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,13 @@
+package com.github.pedroluiznogueira.microservices.currencyconversionservice.proxy;
+
+import com.github.pedroluiznogueira.microservices.currencyconversionservice.model.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(url = "localhost:8000", name = "currency-exchange")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
+}
